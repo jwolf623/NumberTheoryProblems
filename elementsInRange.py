b87d05c9b3d02a26bfbeb19ecd7bebf3758b73bb
jwolf623 <90798582+jwolf623@users.noreply.github.com>
2023-08-22 16:28:50 -0400

    Delete elementsInRange.py

--- elementsInRange.py
+++
@@ -1,6 +0,0 @@
-input_numbers = [int(x) for x in input().split(' ')]
-input_range = [int(x) for x in input().split(' ')]
-
-for number in input_numbers:
-    if input_range[0] <= number <= input_range[1]:
-        print('{}'.format(number), end = ' ')
