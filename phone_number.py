a2909a3d803fe9f550b3f438bf7f77dd60e8d349
jwolf623 <90798582+jwolf623@users.noreply.github.com>
2023-08-22 16:35:33 -0400

    Delete phone_number.py

--- phone_number.py
+++
@@ -1,8 +0,0 @@
-phone_number = int(input())
-
-areaCode=str(phone_number // 10000000)
-firstThree=str((phone_number // 10000) % 1000)
-Lastfour=str(phone_number % 10000)
-
-
-print('({}) {}-{}'.format(areaCode,firstThree,Lastfour))
