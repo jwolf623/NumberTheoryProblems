35ff3ec5cdfbfcc16f742c0e141bbe5dd6318c18
jwolf623 <90798582+jwolf623@users.noreply.github.com>
2023-08-22 16:35:23 -0400

    Delete palindrome.py

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-input_str=input()
-
-input_str_no_spaces = input_str.replace(' ','')
-
-if input_str_no_spaces==input_str_no_spaces[::-1]:
-    print(input_str,'is a palindrome')
-
-else:
-    print(input_str,'is not a palindrome')
