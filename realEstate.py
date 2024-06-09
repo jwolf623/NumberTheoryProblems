67cef6efb47b2a02ac69742a7430c2367c907161
Jordan Wolfson <90798582+jwolf623@users.noreply.github.com>
2024-06-09 13:04:11 -0400

    Delete realEstate.py

--- realEstate.py
+++
@@ -1,9 +0,0 @@
-curr_pr=int(input())
-lastmonth_pr=int(input())
-
-difference=curr_pr - lastmonth_pr
-
-summary_pr = (curr_pr*0.051)/12
-
-print('This house is ${}. The change is ${} since last month.'.format(curr_pr, difference))
-print('The estimated monthly mortgage is ${:.2f}.'.format(summary_pr))
