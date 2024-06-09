dbcfd29b62e842895b81a949914c90a48f6428dd
Jordan Wolfson <90798582+jwolf623@users.noreply.github.com>
2024-06-09 13:02:30 -0400

    Delete caffeineLevel.py

--- caffeineLevel.py
+++
@@ -1,9 +0,0 @@
-caff=float(input())
-
-remainCaff=caff/2
-remainCaff1=remainCaff/2
-remainCaff2=remainCaff1/4
-
-print('After 6 hours: {:.2f} mg'.format(remainCaff))
-print('After 12 hours: {:.2f} mg'.format(remainCaff1))
-print('After 24 hours: {:.2f} mg'.format(remainCaff2))
